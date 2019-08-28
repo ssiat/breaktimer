8095896a3412dd500fe5473a308f8e4d4946ba1f
Pdom <ahb0327@naver.com>
2019-08-29 00:51:09 +0900

    Add clock and timer

---
+++ src/Clock.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import Setting from './Setting.json'
+import Timer from './Timer'
+
+const timeTable = Setting.timeTable;
+
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            time: new Date(),
+            isTimerEnable: false,
+            subject: null
+        }
+    }
+
+    componentDidMount() {
+        setInterval(this.update, 10)
+    }
+
+    update = () => {
+        this.setState({
+            time: new Date()
+        })
+        
+        if ( !this.state.isTimerEnable ) {
+            timeTable.forEach(subject => {
+                if ( ((subject.startHour * 100 + subject.startMinute) <= (this.state.time.getHours() * 100 + this.state.time.getMinutes())) && ((this.state.time.getHours() * 100 + this.state.time.getMinutes()) < (subject.endHour * 100 + subject.endMinute)) ) {
+                    this.setState({
+                        isTimerEnable: true,
+                        subject: subject
+                    })
+                }
+            });
+        } else {
+            if ( ((this.state.subject.startHour * 100 + this.state.subject.startMinute) > (this.state.time.getHours() * 100 + this.state.time.getMinutes())) || ((this.state.time.getHours() * 100 + this.state.time.getMinutes()) >= (this.state.subject.endHour * 100 + this.state.subject.endMinute)) ) {
+                this.setState({
+                    isTimerEnable: false,
+                })
+            }
+        }
+    }
+
+    render() {
+        const h = this.state.time.getHours()
+        const m = this.state.time.getMinutes()
+        const s = this.state.time.getSeconds()
+        const subject = this.state.subject
+
+        if ( this.state.isTimerEnable ) {
+            return (
+                <div>
+                    {h}:{(m < 10 ? '0' + m : m)}:{(s < 10 ? '0' + s : s)}
+                    <h1>{subject.subject}</h1>
+                    <h2>{subject.teacher}</h2>
+                    <Timer endDate={new Date().setHours(subject.endHour, subject.endMinute, 0)} />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    {h}:{(m < 10 ? '0' + m : m)}:{(s < 10 ? '0' + s : s)}
+                </div>
+            )
+        }
+    }
+}
---
+++ src/Timer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Countdown from 'react-countdown-now';
+
+const renderer = ({ hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a completed state
+      return <h1>Done.</h1>;
+    } else {
+      // Render a countdown
+      return <h1>{(minutes < 10 ? '0' + minutes : minutes)}:{(seconds < 10 ? '0' + seconds : seconds)}</h1>;
+    }
+};
+
+export default class Clock extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Countdown
+                date={this.props.endDate}
+                renderer={renderer}
+            />
+        )
+    }
+}
